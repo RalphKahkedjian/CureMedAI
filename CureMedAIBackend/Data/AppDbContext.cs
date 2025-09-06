91b145cee4c0f9fd7d99de31391ef86e617029e9
ralphkian <ralph.kjian@hotmail.com>
2025-09-06 18:38:45 +0300

    Initialized Project

---
+++ CureMedAIBackend/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CureMedAI.CureMedAIBackend.Data
+{
+    internal class AppDbContext
+    {
+        private static string connectionString = @"Data Source=CureMedAI_Database.db;";
+
+    }
+}
